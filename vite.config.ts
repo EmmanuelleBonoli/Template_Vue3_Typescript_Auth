8c383be10ad1535ffc077987318fdc29c5428ff1
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2025-05-21 20:06:59 +0200

    initialize

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite';
+import { PrimeVueResolver } from 'unplugin-vue-components/resolvers';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+      vue(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+      extensions: ['vue'],
+    }),],
+  server: {
+    port: 5421
+  }
+})
